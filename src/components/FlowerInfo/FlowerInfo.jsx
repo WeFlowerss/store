467cb344ef044990f9ada543e4879753b7f4e38e
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-10-16 23:52:42 +0300

    update

--- src/components/FlowerInfo/FlowerInfo.jsx
+++ src/components/FlowerInfo/FlowerInfo.jsx
@@ -1,7 +1,9 @@
+import { Telegram } from "../../helpers/telegram";
 export const FlowerInfo = ({ item }) => {
   return (
     <div>
       <h1>{item.name}</h1>
+      <p>{JSON.stringify(Telegram.data)}</p>
     </div>
   );
 };
