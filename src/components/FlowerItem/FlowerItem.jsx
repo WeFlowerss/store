6b5899754f08c7576b36c9c52112dadcaf5f85e8
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-10-16 18:20:29 +0300

    update

--- src/components/FlowerItem/FlowerItem.jsx
+++ src/components/FlowerItem/FlowerItem.jsx
@@ -2,10 +2,17 @@ import style from "./FlowerItem.module.css";
 
 export const FlowerItem = ({ flower }) => {
   //   const { id, name, image, price, description, compound } = flower;
-  const { name } = flower;
+  const { name, image, id, description } = flower;
   return (
-    <div className={style["flower-item"]}>
-      <h1>{name}</h1>
+    <div className={style["flower-item"]} data-id={id}>
+      <div className={style["flower-body"]}>
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <button>Add To Cart</button>
+      </div>
+      <div className={style["flower-img"]}>
+        <img src={image} alt="bouquet" />
+      </div>
     </div>
   );
 };
