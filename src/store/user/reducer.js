185c1da96275effb7b602d9ba40c133d30af9cfd
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-01-08 17:36:33 +0200

    update

--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -5,9 +5,11 @@ const initialState = {
 export function userReducer(state = initialState, action) {
   switch (action.type) {
     case "UPDATE_USER_ID":
+      const url = new URL(action.payload);
+      const params = url.searchParams;
       return {
         ...state,
-        user: action.payload,
+        user: params,
       };
 
     default:
