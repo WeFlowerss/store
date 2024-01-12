6c1e6d827383227f11c0289bf1234db7421072bb
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-01-12 11:46:39 +0200

    update

--- src/components/BucketItemList/BucketItemList.jsx
+++ src/components/BucketItemList/BucketItemList.jsx
@@ -1,36 +1,17 @@
 /* eslint-disable */
 import React, { useEffect } from "react";
-import { useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchBucket, deleteItemFromBucket } from "../../store/bucket/actions";
 import { BucketItem } from "../BucketItem/BucketItem";
 import style from "./BucketList.module.css";
 
 export const BucketItemList = () => {
-  const tg = window.Telegram.WebApp;
   const user = useSelector((state) => state.userReducer.user);
   const products = useSelector((state) => state.bucketReducer.products);
   const dispatch = useDispatch();
 
-  const onBtnClick = useCallback(() => {
-    const data = {
-      user,
-      products,
-    };
-    tg.sendData(JSON.stringify(data));
-  }, []);
-
   useEffect(() => {
     dispatch(fetchBucket(user?.id));
-    /* tg.onEvent("mainButtonClicked", onBtnClick);
-    tg.mainButton.show();
-    tg.mainButton.setParams({
-      text: "Зробити замовлення",
-      color: "#045661",
-    }); */
-    return () => {
-      // tg.offEvent("mainButtonClicked", onBtnClick);
-    };
   }, []);
 
   const onDeleteClick = (index) => {
@@ -39,7 +20,6 @@ export const BucketItemList = () => {
 
   return (
     <div>
-      <p>{JSON.stringify(tg)}</p>
       <ul className={style.list}>
         {products?.map((product, index) => (
           <BucketItem
