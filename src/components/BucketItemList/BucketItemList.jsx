035704ed0a6a61e3968ba681925aa7720fefc900
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-01-12 11:36:39 +0200

    update

--- src/components/BucketItemList/BucketItemList.jsx
+++ src/components/BucketItemList/BucketItemList.jsx
@@ -1,23 +1,36 @@
 /* eslint-disable */
 import React, { useEffect } from "react";
+import { useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchBucket, deleteItemFromBucket } from "../../store/bucket/actions";
 import { BucketItem } from "../BucketItem/BucketItem";
 import style from "./BucketList.module.css";
 
 export const BucketItemList = () => {
+  const tg = window.Telegram.WebApp;
   const user = useSelector((state) => state.userReducer.user);
   const products = useSelector((state) => state.bucketReducer.products);
   const dispatch = useDispatch();
 
+  const onBtnClick = useCallback(() => {
+    const data = {
+      user,
+      products,
+    };
+    tg.sendData(JSON.stringify(data));
+  }, []);
+
   useEffect(() => {
     dispatch(fetchBucket(user?.id));
+    tg.onEvent("mainButtonClicked", onBtnClick);
+    return () => {
+      tg.offEvent("mainButtonClicked", onBtnClick);
+    };
   }, []);
 
   const onDeleteClick = (index) => {
     dispatch(deleteItemFromBucket(products, user?.id, index));
   };
-  const tg = window.Telegram.WebApp;
   tg.mainButton.show();
   tg.mainButton.setParams({
     text: "Зробити замовлення",
